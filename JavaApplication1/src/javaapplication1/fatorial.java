271b97465669a98f042728398ab2b46e7c26de44
Tayná Oliveira <67444028+olstayna@users.noreply.github.com>
2023-04-05 21:16:30 -0300

    Add files via upload

---
+++ JavaApplication1/src/javaapplication1/fatorial.java
@@ -0,0 +1,17 @@
+package javaapplication1;
+
+public class fatorial {
+    public static void main(String[] args) {
+        for (long i=0;i<=10;i++) {
+            System.out.println(i + "! = " + fatorial(i));
+        }
+    }
+    public static long fatorial (long num) {
+        if (num <=1) {
+            return 1;
+        }
+        else {
+            return num * fatorial(num -1);
+        }
+    }
+}
